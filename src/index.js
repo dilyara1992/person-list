ebef8269e67ded2133abd4c0204b616f4b687fff
dilyara1992 <dilyara.tulegenova@gmail.com>
2020-04-15 01:53:23 -0600

    second commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDom from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Person = ({img,name,job,children}) =>{
+ 
+ const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg` 
+
+  return (
+    <article>
+      <img src={url} 
+      alt="person"/>
+
+      <h4>{name}</h4>
+      <h4>{job}</h4>
+
+    </article>
+  )
+}
+
+
+
+
+const PersonList = () => {
+  return <section>
+    <Person  img="33" name="John" job="developer">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+        laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </Person>
+    <Person  img="23" name="David" job="designer">
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+        laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </Person>
+    <Person  img="56" name="Talgat" job="Analyst">
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+        laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </Person>
+  </section>
+}
+
+
+
+
+
+
+ReactDom.render(<PersonList></PersonList>, document.getElementById('root'));
